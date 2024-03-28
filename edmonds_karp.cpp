95a627654f780ad80105fb25cec7c78456a8bc66
charchalis <charchalis97@gmail.com>
2024-03-28 16:12:21 +0000

    edmonds karp done

--- edmonds_karp.cpp
+++ edmonds_karp.cpp
@@ -1,15 +1,11 @@
-void edmonds_karp(Graph<string> &g){
-    cout << "edmonds karping every city" << endl;
 
-    //TODO
-}
 
 bool edmonds_karp(Graph<string> &g, string city){
 
     if(g.findVertex(city)) cout << "edmonds karping without " << city << endl;
     else{
         cout << "city not found" << endl;       
-        return false;
+        //return false;
     } 
     
     
@@ -63,58 +59,75 @@ bool edmonds_karp(Graph<string> &g, string city){
     for(auto e: sinkVertexEdges){
         e->setFlow(0);
     }
+
+
     
-    //temporario. so pra ver as iteracoes
-    for(int i = 0; i < 1; i++){
-
-
-    map<string,Edge<string> *> discoveryMap; //this will tell the path found by the bfs
-    auto bfsResult = graph.bfs(source_name, discoveryMap); //run bfs
-
-    vector<Edge<string>*> path;
+    cout << endl << "PATHS: " << endl << endl;
     
-    //get path
-    cout << "path: " << endl;
+    while(true){
+        map<string,Edge<string> *> discoveryMap; //this will tell the path found by the bfs
+        auto bfsResult = graph.bfs(source_name, discoveryMap); //run bfs
+                                                               
+        if(!discoveryMap["sink"]) break; //if there is no edge to sink it means that the algorithm is done
 
-    Edge<string>* path_edge = discoveryMap["sink"];
-    cout << "\t" << path_edge->getOrig()->getInfo() << "-" << path_edge->getDest()->getInfo() << endl;
-    Vertex<string>* parent_node = path_edge->getOrig();
-    Vertex<string>* child_node = graph.findVertex("sink");
-    graph.findVertex(parent_node->getInfo())->setPath(path_edge);
 
-    //find max flow of path
-    int maxFlow = path_edge->getWeight() - path_edge->getFlow();
-    while(parent_node->getInfo() != "source"){
-
-        path.push_back(path_edge);
-        child_node = parent_node;
-        path_edge = discoveryMap[parent_node->getInfo()];
-        parent_node = path_edge->getOrig();
-
-        cout << "\t" << path_edge->getOrig()->getInfo() << "-" << path_edge->getDest()->getInfo() << endl;
-
-        graph.findVertex(parent_node->getInfo())->setPath(path_edge);
+        //get path
         
-        int edgeAvailableFlow = path_edge->getWeight() - path_edge->getFlow();
+        Edge<string>* path_edge = discoveryMap["sink"]; //get path's edge to sink
 
-        if(edgeAvailableFlow < maxFlow) maxFlow = edgeAvailableFlow;
+        string path; //this is just to print the path;
+        path = "sink";
+
+        path = path_edge->getOrig()->getInfo() + " -> " + path;
+
+        Vertex<string>* parent_node = path_edge->getOrig(); //get path's parent node of sink
+        Vertex<string>* child_node = graph.findVertex("sink"); //get sink
+        graph.findVertex(parent_node->getInfo())->setPath(path_edge); 
+
+        //find max flow of path
+        int maxFlow = path_edge->getWeight() - path_edge->getFlow();
+        while(parent_node->getInfo() != "source"){
+            child_node = parent_node;
+            path_edge = discoveryMap[parent_node->getInfo()];
+            parent_node = path_edge->getOrig();
+
+            path = path_edge->getOrig()->getInfo() + " -> " + path;
+
+            graph.findVertex(parent_node->getInfo())->setPath(path_edge);
+            
+            int edgeAvailableFlow = path_edge->getWeight() - path_edge->getFlow();
+
+            if(edgeAvailableFlow < maxFlow) maxFlow = edgeAvailableFlow;
+        }
+        cout << path;
+        cout << "\t(flow: " << maxFlow << ")" << endl;
+
+
+
+
+        
+        //set flow of path
+        Vertex<string>* node = graph.findVertex("source");
+        while(node->getInfo() != "sink"){
+            auto edge = node->getPath();
+            edge->setFlow(edge->getFlow() + maxFlow);
+            node = edge->getDest();
+        }
+
+        //print graph with flow
+        /*verti = graph.getVertexSet();
+        for(auto v: verti){
+            auto edges = v->getAdj();
+            for(auto e: edges){
+                cout << e->getOrig()->getInfo() << "-" << e->getDest()->getInfo() << " -> " << e->getFlow() << endl;
+            }
+        }*/
+
     }
-    cout << "flow: " << maxFlow << endl;
 
-
-
-
-    
-    //set flow of path
-    Vertex<string>* node = graph.findVertex("source");
-    while(node->getInfo() != "sink"){
-        auto edge = node->getPath();
-        edge->setFlow(maxFlow);
-        node = edge->getDest();
-    }
-
+    //print graph with flow
+    cout << endl;
     verti = graph.getVertexSet();
-    
     for(auto v: verti){
         auto edges = v->getAdj();
         for(auto e: edges){
@@ -122,9 +135,15 @@ bool edmonds_karp(Graph<string> &g, string city){
         }
     }
 
-    }
 
-    //TODO
+
     
     return true;
+}
+
+void edmonds_karp(Graph<string> &g){
+    cout << "edmonds karping every city" << endl;
+
+    edmonds_karp(g, "none");
+    
 }
